7c1ef98171298005bfc040274b352ef7c22f34ca
Vishal Shetty <vishalshetty1990@gmail.com>
2019-05-16 17:02:11 +0530

    Added state for form management

--- client/src/Components/AddBook.js
+++ client/src/Components/AddBook.js
@@ -6,6 +6,16 @@ import {getAuthorsQuery} from './../queries';
 
 class AddBook extends React.Component {
 
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      genre: "",
+      authorid: ""
+    };
+
+  }
+
   displayAuthors = () => {
       const {data} = this.props;
       if(data.loading)
@@ -15,23 +25,29 @@ class AddBook extends React.Component {
       }
   }
 
+  formOnSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('from state',this.state);
+  }
+
   render() {
-    console.log(this.props);
+    const {name, genre, authorid} = this.state;
     return(
-      <form>
+      <form onSubmit={this.formOnSubmit}>
         <div>
           <label>Book Name</label>
-          <input type="text" name="bookname"/>
+          <input type="text" value={name} onChange={(e) => this.setState({name: e.target.value})}/>
         </div>
 
         <div>
           <label>Book Genre</label>
-          <input type="text" name="bookgenre"/>
+          <input type="text" value = {genre} onChange={(e) => this.setState({genre: e.target.value})}/>
         </div>
 
         <div>
           <label>Author</label>
-          <select>
+          <select value={authorid} onChange={(e) => this.setState({authorid: e.target.value})}>
             <option>Select Author</option>
             {this.displayAuthors()}
           </select>
